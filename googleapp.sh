e4aabbb9f8c78f279bcb979aa9d054071517f815
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2020-04-04 22:24:26 +0700

    Add files via upload

---
+++ googleapp.sh
@@ -0,0 +1,95 @@
+#! bin/sh
+
+function version {
+	echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'
+}
+
+rm /sdcard/Download/ext/temp/c 2>/dev/null
+rm /sdcard/Download/ext/temp/c1 2>/dev/null
+rm /sdcard/Download/ext/temp/META-INF 2>/dev/null
+rm /sdcard/Download/ext/temp/system 2>/dev/null
+
+unzip -qq -o /sdcard/tweak-havoc-capricorn-q-part2.zip -d /sdcard/Download/ext/temp
+
+aapt dump badging /data/app/com.google.android.contacts-*/base.apk > /sdcard/Download/ext/temp/c
+a=`cat /sdcard/Download/ext/temp/c | grep "versionName" | sed "s+' platform.*++g; s+^.*' versionName='++g"`
+
+aapt dump badging /sdcard/Download/ext/temp/system/product/priv-app/Contacts/base.apk > /sdcard/Download/ext/temp/c1
+b=`cat /sdcard/Download/ext/temp/c1 | grep "versionName" | sed "s+' platform.*++g; s+^.*' versionName='++g"`
+
+if [ $(version $a) -gt $(version $b) ]
+then
+	rm -rf /sdcard/Download/ext/temp/system/product/priv-app/Contacts/*
+	cp -r /data/app/com.google.android.contacts-*/* /sdcard/Download/ext/temp/system/product/priv-app/Contacts
+	echo "•Google_Contacts-$b --> Google_Contacts-$a" >> /sdcard/Download/ext/temp/log
+	echo "1" >> /sdcard/Download/ext/temp/confirm
+else
+	echo "•Google Contacts: Không có cập nhật mới." >> /sdcard/Download/ext/temp/log
+	echo "0" >> /sdcard/Download/ext/temp/confirm
+fi
+
+rm /sdcard/Download/ext/temp/c 2>/dev/null
+rm /sdcard/Download/ext/temp/c1 2>/dev/null
+
+
+rm /sdcard/Download/ext/temp/d 2>/dev/null
+rm /sdcard/Download/ext/temp/d1 2>/dev/null
+
+aapt dump badging /data/app/com.google.android.dialer-*/base.apk > /sdcard/Download/ext/temp/d
+a=`cat /sdcard/Download/ext/temp/d | grep "versionName" | sed "s+' platform.*++g; s+^.*' versionName='++g"`
+
+aapt dump badging /sdcard/Download/ext/temp/system/product/priv-app/Dialer/base.apk > /sdcard/Download/ext/temp/d1
+b=`cat /sdcard/Download/ext/temp/d1 | grep "versionName" | sed "s+' platform.*++g; s+^.*' versionName='++g"`
+
+if [ $(version $a) -gt $(version $b) ]
+then
+	rm -rf /sdcard/Download/ext/temp/system/product/priv-app/Dialer/*
+	cp -r /data/app/com.google.android.dialer-*/* /sdcard/Download/ext/temp/system/product/priv-app/Dialer
+	echo "•Google_Dialer-$b --> Google_Dialer-$a" >> /sdcard/Download/ext/temp/log
+	echo "1" >> /sdcard/Download/ext/temp/confirm
+else
+	echo "•Google Dialer: Không có cập nhật mới." >> /sdcard/Download/ext/temp/log
+	echo "0" >> /sdcard/Download/ext/temp/confirm
+fi
+
+rm /sdcard/Download/ext/temp/d 2>/dev/null
+rm /sdcard/Download/ext/temp/d1 2>/dev/null
+
+
+rm /sdcard/Download/ext/temp/m 2>/dev/null
+rm /sdcard/Download/ext/temp/m1 2>/dev/null
+
+aapt dump badging /data/app/com.google.android.apps.messaging-*/base.apk > /sdcard/Download/ext/temp/m
+b=`cat /sdcard/Download/ext/temp/m | grep "versionCode" | sed "s+' versionName.*++g; s+package: name='com.google.android.apps.messaging' versionCode='++g"`
+d=`cat /sdcard/Download/ext/temp/m | grep "versionCode" | sed "s+' platform.*++g; s+^.*' versionName='++g"`
+
+aapt dump badging /sdcard/Download/ext/temp/system/product/app/messaging/base.apk > /sdcard/Download/ext/temp/m1
+b=`cat /sdcard/Download/ext/temp/m1 | grep "versionCode" | sed "s+' versionName.*++g; s+package: name='com.google.android.apps.messaging' versionCode='++g"`
+d=`cat /sdcard/Download/ext/temp/m1 | grep "versionCode" | sed "s+' platform.*++g; s+^.*' versionName='++g"`
+
+if [ $(version $a) -gt $(version $b) ]
+then
+	rm -rf /sdcard/Download/ext/temp/system/product/app/messaging/*
+	cp -r /data/app/com.google.android.apps.messaging-*/* /sdcard/Download/ext/temp/system/product/app/messaging
+	echo "•Google_Messages-$d --> Google_Messages-$c" >> /sdcard/Download/ext/temp/log
+	echo "1" >> /sdcard/Download/ext/temp/confirm
+else
+	echo "•Google Messages: Không có cập nhật mới." >> /sdcard/Download/ext/temp/log
+	echo "0" >> /sdcard/Download/ext/temp/confirm
+fi
+
+rm /sdcard/Download/ext/temp/m 2>/dev/null
+rm /sdcard/Download/ext/temp/m1 2>/dev/null
+
+
+if grep -Fxq "1" /sdcard/Download/ext/temp/confirm
+then
+	cd /sdcard/Download/ext/temp
+	zip -r tweak-havoc-capricorn-q-part2.zip META-INF system
+	rm /sdcard/tweak-havoc-capricorn-q-part2.zip
+	mv -f /sdcard/Download/ext/temp/tweak-havoc-capricorn-q-part2.zip /sdcard
+fi
+
+rm /sdcard/Download/ext/temp/confirm
+rm -rf /sdcard/Download/ext/temp/META-INF 2>/dev/null
+rm -rf /sdcard/Download/ext/temp/system 2>/dev/null
---
+++ kernel.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+echo "                   KERNEL INFOMATION"
+echo "•CPU:"
+echo "-Core 0:"
+echo "   CPU Maximum Frequency: `cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq` MHz"
+echo "   CPU Minimum Frequency: `cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq` MHz"
+echo "   Governor: `cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor`"
+
+echo "-Core 1:"
+echo "   CPU Maximum Frequency: `cat /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq` MHz"
+echo "   CPU Minimum Frequency: `cat /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq` MHz"
+echo "   Governor: `cat /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor`"
+
+echo "-Core 2:"
+echo "   CPU Maximum Frequency: `cat /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq` MHz"
+echo "   CPU Minimum Frequency: `cat /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq` MHz"
+echo "   Governor: `cat /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor`"
+
+echo "-Core 3:"
+echo "   CPU Maximum Frequency: `cat /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq` MHz"
+echo "   CPU Minimum Frequency: `cat /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq` MHz"
+echo "   Governor: `cat /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor`"
+
+echo "-CPU Boost:"
+echo "   Input Interval: `cat /sys/module/cpu_boost/parameters/input_boost_ms` ms"
+echo "   Input Boost Frequency:"
+echo "      Core 1: `cat /sys/module/cpu_boost/parameters/input_boost_freq | sed 's+ 1:.*++g; s+0:++g'` MHz"
+echo "      Core 2: `cat /sys/module/cpu_boost/parameters/input_boost_freq | sed 's+ 2:.*++g; s+^.*1:++g'` MHz"
+echo "      Core 3: `cat /sys/module/cpu_boost/parameters/input_boost_freq | sed 's+ 3:.*++g; s+^.*2:++g'` MHz"
+echo "      Core 3: `cat /sys/module/cpu_boost/parameters/input_boost_freq | sed 's+^.*3:++g'`MHz"
+
+echo "•GPU:"
+echo "-GPU Governor: `cat /sys/class/kgsl/kgsl-3d0/devfreq/governor`"
+
+echo "•THERMAL:"
+a=`cat /sys/module/msm_thermal/core_control/enabled`
+if [ $a  == 1 ]
+then
+	echo "-Core Control: On"
+else
+	echo "-Core Control: Off"
+fi
+a=`cat /sys/module/msm_thermal/parameters/enabled`
+if [ $a  == Y ]
+then
+	echo "-Temperature Throttle: On"
+else
+	echo "-Temperature Throttle: Off"
+fi
+
+echo "•I/O Scheduler:"
+echo "-Scheduler: `awk 'NR > 1 {print $1}' RS='[' FS=']' /sys/block/sda/queue/scheduler`"
+echo "-Read-ahead: `cat /sys/block/sda/queue/read_ahead_kb` kB"
+
+echo "•Vitural Memory:"
+echo "-Drop caches: `cat /proc/sys/vm/drop_caches`"
+swap_total=`cat /proc/meminfo | grep "SwapTotal" | sed 's/[^0-9]*//g'`
+zram_total=`awk -v var1=$swap_total -v var2=1024 'BEGIN { print  ( var1 / var2 ) }'`
+
+swap_free=`cat /proc/meminfo | grep "SwapFree" | sed 's/[^0-9]*//g'`
+zram_free=`awk -v var1=$swap_free -v var2=1024 'BEGIN { print  ( var1 / var2 ) }'`
+
+zram_used=`awk -v var1=$zram_total -v var2=$zram_free 'BEGIN { print  ( var1 - var2 ) }'`
+
+echo "-Zram Total: $zram_total MB"
+echo "-Zram Used: $zram_used MB"
+echo "-Zram Free: $zram_free MB"
+echo "-Swappiness: `cat /proc/sys/vm/swappiness`"
+echo "•MISC:"
+fsync=`cat /sys/kernel/dyn_fsync/Dyn_fsync_active`
+if [ $fsync == 1 ]
+then
+	echo "-Fsync: Off"
+else
+	echo "-Fsync: On"
+fi
+
+read -r -s -p $'Nhấn Enter để tiếp tục...\n'
