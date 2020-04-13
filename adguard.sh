609000d74d0bcb978b340e6dce4c25efafbf9966
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2020-04-13 10:41:22 +0700

    Update adguard.sh

--- adguard.sh
+++ adguard.sh
@@ -7,7 +7,7 @@ wget -O - https://a2zapk.com/Search/Adguard/user=akJ1bUhJZ3dKcmVIR2FXWmdpTmNJUnB
 a=`cat /sdcard/Download/ext/temp/a | grep "Adguard - Block Ads Without Root" | perl -pe 'if(($_)=/([0-9]+([.][0-9]+)+)/){$_.="\n"}' | awk 'min == "" || $1<min{min=$1} $1>max{max=$1} END{print max}'`
 
 aapt dump badging /data/app/com.adguard.android*/*.apk > /sdcard/Download/ext/temp/a1
-b=`cat /sdcard/Download/ext/temp/a1 | grep "versionName" | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/'`
+b=`cat /sdcard/Download/ext/temp/a1 | perl -pe 'if(($_)=/([0-9]+([.][0-9]+)+)/){$_.="\n"}'`
 
 function version {
 	echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'
