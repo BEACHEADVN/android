a7c341d73cbcb471654cc9a86a40f18ed52359a7
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2020-04-13 10:54:52 +0700

    Update skm.sh

--- skm.sh
+++ skm.sh
@@ -4,14 +4,14 @@ rm /sdcard/Download/ext/temp/s 2>/dev/null
 rm /sdcard/Download/ext/temp/s1 2>/dev/null
 
 wget -O - https://github.com/SmartPack/SmartPack-Kernel-Manager/releases > /sdcard/Download/ext/temp/s
-a=`cat /sdcard/Download/ext/temp/s | grep "Release tag:" | sed '1!d' | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/'`
+a=`cat /sdcard/Download/ext/temp/s | grep "Release tag:" | perl -pe 'if(($_)=/([0-9]+([.][0-9]+)+)/){$_.="\n"}' | awk 'min == "" || $1<min{min=$1} $1>max{max=$1} END{print max}'`
 
 aapt dump badging /data/app/com.smartpack.kernelmanager*/base.apk > /sdcard/Download/ext/temp/s1 2>/dev/null
 if [[ -z $(grep '[^[:space:]]' /sdcard/Download/ext/temp/s1) ]]
 then
 	aapt dump badging /sdcard/Download/com.smartpack.kernelmanager*.apk > /sdcard/Download/ext/temp/s1 2>/dev/null
 fi
-b=`cat /sdcard/Download/ext/temp/s1 | grep "versionName" | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/'`
+b=`cat /sdcard/Download/ext/temp/s1 | perl -pe 'if(($_)=/([0-9]+([.][0-9]+)+)/){$_.="\n"}'`
 
 function version {
 	echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'
