ee1e31f458872be8e8241641f132a85e9190e65f
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2020-04-19 10:07:29 +0700

    Update battery.sh

--- battery.sh
+++ battery.sh
@@ -1,21 +1,28 @@
 #!/bin/sh -e
 
-echo 0 > /sys/module/msm_thermal/core_control/enabled
-echo N > /sys/module/msm_thermal/parameters/enabled
 chmod 777 /sys/devices/system/cpu/cpu0/cpufreq/*
 echo 1209600 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo 307200 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo pwrutilx > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 chmod 777 /sys/devices/system/cpu/cpu1/cpufreq/*
 echo 1209600 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+echo 307200 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+echo pwrutilx > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 chmod 777 /sys/devices/system/cpu/cpu2/cpufreq/*
 echo 1209600 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+echo 307200 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
+echo pwrutilx > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
 chmod 777 /sys/devices/system/cpu/cpu3/cpufreq/*
 echo 1209600 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
-echo 10 > /sys/module/cpu_boost/parameters/input_boost_ms
-echo '0:307200 1:307200 2:307200 3:307200' > /sys/module/cpu_boost/parameters/input_boost_freq
+echo 307200 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
+echo pwrutilx > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+chmod 777 /sys/module/cpu_input_boost/parameters/*
+echo 307200 > /sys/module/cpu_input_boost/parameters/input_boost_freq_lp
+echo 307200 > /sys/module/cpu_input_boost/parameters/input_boost_freq_hp
 echo powersave > /sys/class/kgsl/kgsl-3d0/devfreq/governor
-echo 0 > /sys/kernel/dyn_fsync/Dyn_fsync_active
-echo 60 > /proc/sys/vm/swappiness
 echo noop > /sys/block/sda/queue/scheduler
+echo 10 > /sys/module/cpu_input_boost/parameters/input_boost_duration
 echo 128 > /sys/block/sda/queue/read_ahead_kb
-echo 1 > /sys/module/msm_thermal/core_control/enabled
-echo Y > /sys/module/msm_thermal/parameters/enabled
+sync
+echo 3 > /proc/sys/vm/drop_caches
+echo 0 > /sys/kernel/dyn_fsync/Dyn_fsync_active
