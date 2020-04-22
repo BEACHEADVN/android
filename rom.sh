db250424e9657ebbc2af50b8025ea6070a41cef8
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2020-04-22 21:10:51 +0700

    Add files via upload

---
+++ rom.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+rm -rf /sdcard/Download/ext/temp/r 2>/dev/null
+
+wget --no-check-certificate -O - https://osdn.net/projects/henma-p/storage/capricorn/ > /sdcard/Download/ext/temp/r
+a=`cat /sdcard/Download/ext/temp/r | grep "Havoc-OS" | grep "href" | sed '/GApps/d' | grep -Eo '[0-9]+' | sort -rn | head -n 1`
+
+b=`find /storage/emulated/0 -maxdepth 1 -type f -name "Havoc-OS*" | grep -Eo '[0-9]+' | sort -rn | head -n 1`
+
+if [ $a -gt $b [
+then
+	download_link=`cat /sdcard/Download/ext/temp/r | grep "Havoc-OS" | grep "href" | sed '/GApps/d' | sed 's+^.*href="+https://osdn.net+g; s+">++g'`
+	wget -P /sdcard "$download_link"
+	echo "•Havoc OS: Không có cập nhật mới." >> /sdcard/Download/ext/temp/log
+fi
+else
+	echo "•Havoc OS: Không có cập nhật mới." >> /sdcard/Download/ext/temp/log
+fi
+fi
+
+rm -rf /sdcard/Download/ext/temp/r 2>/dev/null
