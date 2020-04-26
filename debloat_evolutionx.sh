683bfb4d296ab95cd3d2c5d37c0d6bec9aa7bc0a
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2020-04-26 18:53:55 +0700

    Add files via upload

---
+++ debloat_evolutionx.sh
@@ -0,0 +1,78 @@
+rm -rf /sdcard/Download/ext/temp/META-INF 2>/dev/null
+rm -rf /sdcard/Download/ext/temp/system 2>/dev/null
+rm /sdcard/Download/ext/temp/remove1 2>/dev/null
+rm /sdcard/Download/ext/temp/remove2 2>/dev/null
+rm /sdcard/Download/ext/temp/remove3 2>/dev/null
+rm /sdcard/Download/ext/temp/remove4 2>/dev/null
+rm /sdcard/Download/ext/temp/remove5 2>/dev/null
+rm /sdcard/Download/ext/temp/remove6 2>/dev/null
+rm /sdcard/Download/ext/temp/zip 2>/dev/null
+rm /sdcard/Download/ext/temp/zip1 2>/dev/null
+rm /sdcard/Download/ext/temp/zip2 2>/dev/null
+rm /sdcard/Download/ext/temp/checkpath 2>/dev/null
+
+unzip -qq -o /sdcard/tweak-evolutionx-capricorn-q-part1.zip -d /sdcard/Download/ext/temp
+cat /sdcard/Download/ext/temp/META-INF/com/google/android/updater-script > /sdcard/Download/ext/temp/remove1
+awk -i inplace 'NR>y+x{print A[NR%y]} {A[NR%y]=$0}' x=37 y=17 /sdcard/Download/ext/temp/remove1
+cat /sdcard/Download/ext/temp/remove1 | sort >> /sdcard/Download/ext/temp/remove2
+
+sed -n -e '1,37p' /sdcard/Download/ext/temp/META-INF/com/google/android/updater-script > /sdcard/Download/ext/temp/updater-script
+cat /sdcard/Download/ext/temp/remove2 >> /sdcard/Download/ext/temp/updater-script
+tail -n17 /sdcard/Download/ext/temp/META-INF/com/google/android/updater-script >> /sdcard/Download/ext/temp/updater-script
+rm /sdcard/Download/ext/temp/META-INF/com/google/android/updater-script 2>/dev/null
+mv /sdcard/Download/ext/temp/updater-script /sdcard/Download/ext/temp/META-INF/com/google/android
+cd /sdcard/Download/ext/temp
+path=`find /sdcard/Download/ext -name "EvolutionX_*"`
+if [ -d $path/system/system/product/priv-app/SettingsIntelligenceGooglePrebuilt ]
+then
+echo ""
+else
+rm -rf /sdcard/Download/ext/temp/system/product/priv-app/SettingsIntelligenceGooglePrebuilt
+fi
+zip -r tweak-evolutionx-capricorn-q-part1.zip META-INF system
+rm /sdcard/tweak-evolutionx-capricorn-q-part1.zip 2>/dev/null
+mv -f /sdcard/Download/ext/temp/tweak-evolutionx-capricorn-q-part1.zip /sdcard
+awk -i inplace '{print substr($0,2)}' /sdcard/Download/ext/temp/remove2
+sed -i 's+..$++g' /sdcard/Download/ext/temp/remove2
+find /sdcard/Download/ext -name "EvolutionX_*" > /sdcard/Download/ext/temp/zip1
+awk -i inplace '$0="rm -rf "$0' /sdcard/Download/ext/temp/zip1
+remove=`cat /sdcard/Download/ext/temp/zip1`
+awk -v remove="$remove" '{print remove $0}' /sdcard/Download/ext/temp/remove2 > /sdcard/Download/ext/temp/remove3
+bash /sdcard/Download/ext/temp/remove3 2>/dev/null
+
+rm -rf /sdcard/Download/ext/temp/META-INF 2>/dev/null
+rm -rf /sdcard/Download/ext/temp/system 2>/dev/null
+
+
+unzip -qq -o /sdcard/tweak-evolutionx-capricorn-q-part2.zip -d /sdcard/Download/ext/temp
+cat /sdcard/Download/ext/temp/META-INF/com/google/android/updater-script > /sdcard/Download/ext/temp/remove4
+awk -i inplace 'NR>y+x{print A[NR%y]} {A[NR%y]=$0}' x=37 y=12 /sdcard/Download/ext/temp/remove4
+cat /sdcard/Download/ext/temp/remove4 | sort >> /sdcard/Download/ext/temp/remove5
+sed -n -e '1,37p' /sdcard/Download/ext/temp/META-INF/com/google/android/updater-script > /sdcard/Download/ext/temp/updater-script
+cat /sdcard/Download/ext/temp/remove5 >> /sdcard/Download/ext/temp/updater-script
+tail -n12 /sdcard/Download/ext/temp/META-INF/com/google/android/updater-script >> /sdcard/Download/ext/temp/updater-script
+rm /sdcard/Download/ext/temp/META-INF/com/google/android/updater-script 2>/dev/null
+mv /sdcard/Download/ext/temp/updater-script /sdcard/Download/ext/temp/META-INF/com/google/android
+zip -r tweak-evolutionx-capricorn-q-part2.zip META-INF
+rm /sdcard/tweak-evolutionx-capricorn-q-part2.zip
+mv -f /sdcard/Download/ext/temp/tweak-evolutionx-capricorn-q-part2.zip /sdcard
+
+awk -i inplace '{print substr($0,2)}' /sdcard/Download/ext/temp/remove5
+sed -i 's+..$++g' /sdcard/Download/ext/temp/remove5
+find /sdcard/Download/ext -name "EvolutionX_*" > /sdcard/Download/ext/temp/zip2
+awk -i inplace '$0="rm -rf "$0' /sdcard/Download/ext/temp/zip2
+remove=`cat /sdcard/Download/ext/temp/zip2`
+awk -v remove="$remove" '{print remove $0}' /sdcard/Download/ext/temp/remove5 > /sdcard/Download/ext/temp/remove6
+bash /sdcard/Download/ext/temp/remove6 2>/dev/null
+
+rm -rf /sdcard/Download/ext/temp/META-INF 2>/dev/null
+rm -rf /sdcard/Download/ext/temp/system 2>/dev/null
+rm /sdcard/Download/ext/temp/remove1 2>/dev/null
+rm /sdcard/Download/ext/temp/remove2 2>/dev/null
+rm /sdcard/Download/ext/temp/remove3 2>/dev/null
+rm /sdcard/Download/ext/temp/remove4 2>/dev/null
+rm /sdcard/Download/ext/temp/remove5 2>/dev/null
+rm /sdcard/Download/ext/temp/remove6 2>/dev/null
+rm /sdcard/Download/ext/temp/zip 2>/dev/null
+rm /sdcard/Download/ext/temp/zip1 2>/dev/null
+rm /sdcard/Download/ext/temp/zip2 2>/dev/null
