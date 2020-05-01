98874d8a5eea5938f038f6f3ca38d1058654e214
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2020-05-01 09:08:04 +0700

    Add files via upload

--- unpack.sh
+++ unpack.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 rm /sdcard/Download/ext/temp/zip 2>/dev/null
 cd /sdcard
 find . -name "*.zip" >> /sdcard/Download/ext/temp/zip
@@ -124,85 +125,16 @@ else
 	
 	case $debloat in
 		1)
-			rm -rf /sdcard/Download/ext/temp/META-INF 2>/dev/null
-			rm -rf /sdcard/Download/ext/temp/system 2>/dev/null
-			rm /sdcard/Download/ext/temp/remove1 2>/dev/null
-			rm /sdcard/Download/ext/temp/remove2 2>/dev/null
-			rm /sdcard/Download/ext/temp/remove3 2>/dev/null
-			rm /sdcard/Download/ext/temp/remove4 2>/dev/null
-			rm /sdcard/Download/ext/temp/remove5 2>/dev/null
-			rm /sdcard/Download/ext/temp/remove6 2>/dev/null
-			rm /sdcard/Download/ext/temp/zip 2>/dev/null
-			rm /sdcard/Download/ext/temp/zip1 2>/dev/null
-			rm /sdcard/Download/ext/temp/zip2 2>/dev/null
-			rm /sdcard/Download/ext/temp/checkpath 2>/dev/null
-			
-			unzip -qq -o /sdcard/tweak-havoc-capricorn-q-part1.zip -d /sdcard/Download/ext/temp
-			cat /sdcard/Download/ext/temp/META-INF/com/google/android/updater-script > /sdcard/Download/ext/temp/remove1
-			awk -i inplace 'NR>y+x{print A[NR%y]} {A[NR%y]=$0}' x=37 y=19 /sdcard/Download/ext/temp/remove1
-			cat /sdcard/Download/ext/temp/remove1 | sort >> /sdcard/Download/ext/temp/remove2
-			
-			sed -n -e '1,37p' /sdcard/Download/ext/temp/META-INF/com/google/android/updater-script > /sdcard/Download/ext/temp/updater-script
-			cat /sdcard/Download/ext/temp/remove2 >> /sdcard/Download/ext/temp/updater-script
-			tail -n19 /sdcard/Download/ext/temp/META-INF/com/google/android/updater-script >> /sdcard/Download/ext/temp/updater-script
-			rm /sdcard/Download/ext/temp/META-INF/com/google/android/updater-script 2>/dev/null
-			mv /sdcard/Download/ext/temp/updater-script /sdcard/Download/ext/temp/META-INF/com/google/android
-			cd /sdcard/Download/ext/temp
-                        find /sdcard/Download/ext -name "Havoc-OS-v3.*" > /sdcard/Download/ext/temp/checkpath
-			path=`cat /sdcard/Download/ext/temp/checkpath`
-			if [ -d $path/system/system/product/priv-app/SettingsIntelligenceGooglePrebuilt ]
-			then
-				echo ""
-			else
-				rm -rf /sdcard/Download/ext/temp/system/product/priv-app
-			fi
-			zip -r tweak-havoc-capricorn-q-part1.zip META-INF system
-			rm /sdcard/tweak-havoc-capricorn-q-part1.zip 2>/dev/null
-			mv -f /sdcard/Download/ext/temp/tweak-havoc-capricorn-q-part1.zip /sdcard
-			awk -i inplace '{print substr($0,2)}' /sdcard/Download/ext/temp/remove2
-			sed -i 's+..$++g' /sdcard/Download/ext/temp/remove2
-			find /sdcard/Download/ext -name "Havoc-OS*" > /sdcard/Download/ext/temp/zip1
-			awk -i inplace '$0="rm -rf "$0' /sdcard/Download/ext/temp/zip1
-			remove=`cat /sdcard/Download/ext/temp/zip1`
-			awk -v remove="$remove" '{print remove $0}' /sdcard/Download/ext/temp/remove2 > /sdcard/Download/ext/temp/remove3
-			bash /sdcard/Download/ext/temp/remove3 2>/dev/null
-			
-			rm -rf /sdcard/Download/ext/temp/META-INF 2>/dev/null
-			rm -rf /sdcard/Download/ext/temp/system 2>/dev/null
-			
-
-			unzip -qq -o /sdcard/tweak-havoc-capricorn-q-part2.zip -d /sdcard/Download/ext/temp
-			cat /sdcard/Download/ext/temp/META-INF/com/google/android/updater-script > /sdcard/Download/ext/temp/remove4
-			awk -i inplace 'NR>y+x{print A[NR%y]} {A[NR%y]=$0}' x=37 y=15 /sdcard/Download/ext/temp/remove4
-			cat /sdcard/Download/ext/temp/remove4 | sort >> /sdcard/Download/ext/temp/remove5
-			sed -n -e '1,37p' /sdcard/Download/ext/temp/META-INF/com/google/android/updater-script > /sdcard/Download/ext/temp/updater-script
-			cat /sdcard/Download/ext/temp/remove5 >> /sdcard/Download/ext/temp/updater-script
-			tail -n15 /sdcard/Download/ext/temp/META-INF/com/google/android/updater-script >> /sdcard/Download/ext/temp/updater-script
-			rm /sdcard/Download/ext/temp/META-INF/com/google/android/updater-script 2>/dev/null
-			mv /sdcard/Download/ext/temp/updater-script /sdcard/Download/ext/temp/META-INF/com/google/android
-			zip -r tweak-havoc-capricorn-q-part2.zip META-INF system
-			rm /sdcard/tweak-havoc-capricorn-q-part2.zip
-			mv -f /sdcard/Download/ext/temp/tweak-havoc-capricorn-q-part2.zip /sdcard
-			
-			awk -i inplace '{print substr($0,2)}' /sdcard/Download/ext/temp/remove5
-			sed -i 's+..$++g' /sdcard/Download/ext/temp/remove5
-			find /sdcard/Download/ext -name "Havoc-OS*" > /sdcard/Download/ext/temp/zip2
-			awk -i inplace '$0="rm -rf "$0' /sdcard/Download/ext/temp/zip2
-			remove=`cat /sdcard/Download/ext/temp/zip2`
-			awk -v remove="$remove" '{print remove $0}' /sdcard/Download/ext/temp/remove5 > /sdcard/Download/ext/temp/remove6
-			bash /sdcard/Download/ext/temp/remove6 2>/dev/null
-			
-			rm -rf /sdcard/Download/ext/temp/META-INF 2>/dev/null
-			rm -rf /sdcard/Download/ext/temp/system 2>/dev/null
-			rm /sdcard/Download/ext/temp/remove1 2>/dev/null
-			rm /sdcard/Download/ext/temp/remove2 2>/dev/null
-			rm /sdcard/Download/ext/temp/remove3 2>/dev/null
-			rm /sdcard/Download/ext/temp/remove4 2>/dev/null
-			rm /sdcard/Download/ext/temp/remove5 2>/dev/null
-			rm /sdcard/Download/ext/temp/remove6 2>/dev/null
-			rm /sdcard/Download/ext/temp/zip 2>/dev/null
-			rm /sdcard/Download/ext/temp/zip1 2>/dev/null
-			rm /sdcard/Download/ext/temp/zip2 2>/dev/null
+			case "$name" in 
+				*Havoc*)
+					bash sig_havoc.sh
+					bash debloat_havoc.sh
+				;;
+				*EvolutionX*)
+					bash sig_evolutionx.sh
+					bash debloat_evolutionx.sh
+				;;
+			esac
 		;;
 		2)
 			echo "Bỏ qua Debloat"
@@ -217,4 +149,3 @@ rm /sdcard/Download/ext/temp/rom 2>/dev/null
 rm /sdcard/Download/ext/temp/romlist 2>/dev/null
 rm /sdcard/Download/ext/temp/romlist1 2>/dev/null
 rm -rf /sdcard/Download/ext/temp/__pycache__ 2>/dev/null
-rm /sdcard/Download/ext/temp/checkpath 2>/dev/null
