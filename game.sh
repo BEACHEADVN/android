ddfb4dd4fd65d391eb33e60beeee7afb285a9026
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2020-05-29 20:07:19 +0700

    Update game.sh

--- game.sh
+++ game.sh
@@ -65,6 +65,8 @@ case $game in
 		pm disable g7.lockscreenroot
 		pm disable org.telegram.messenger
 		pm enable com.vng.inputmethod.labankey
+pm enable catch_.me_.if_.you_.can_
+am start -a android.intent.action.MAIN -n catch_.me_.if_.you_.can_/catch_.me_.if_.you_.can_.MainActivity
 		pm enable com.kleientertainment.doNotStarvePocket
 		pm disable com.facebook.games
 		am start -a android.intent.action.MAIN -n com.kleientertainment.doNotStarvePocket/com.kleientertainment.doNotStarvePocket.DoNotStarveActivity
