d37b343faf98fa4b86f8dcba9ba16356e20ef7b2
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2021-01-08 21:48:27 +0700

    Update gi.sh

--- gi.sh
+++ gi.sh
@@ -2,9 +2,9 @@ su -c "chmod 777 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
 su -c "echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
 su -c "chmod 777 /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor"
 su -c "echo performance > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor"
-su -c "chmod 777 /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+su -c "chmod 777 /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor"
 su -c "echo performance > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor"
-su -c "chmod 777 /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+su -c "chmod 777 /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor"
 su -c "echo performance > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor"
 su -c "chmod 777 /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor"
 su -c "echo performance > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor"
