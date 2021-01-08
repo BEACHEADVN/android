463a821b8f1ddbbcb0e33cccf88b99c5dff9e8c5
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2021-01-08 19:55:55 +0700

    Add files via upload

--- bt.sh
+++ bt.sh
@@ -1,32 +1,32 @@
-chmod 777 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo conservative > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-chmod 777 /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-echo conservative > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-chmod 777 /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-echo conservative > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-chmod 777 /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-echo conservative > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-chmod 777 /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
-echo conservative > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
-chmod 777 /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor
-echo conservative > /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor
-chmod 777 /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor
-echo conservative > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor
-chmod 777 /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor
-echo conservative > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor
+su -c "chmod 777 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
+su -c "echo conservative > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
+su -c "chmod 777 /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor"
+su -c "echo conservative > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor"
+su -c "chmod 777 /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor"
+su -c "echo conservative > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor"
+su -c "chmod 777 /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor"
+su -c "echo conservative > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor"
+su -c "chmod 777 /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor"
+su -c "echo conservative > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor"
+su -c "chmod 777 /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor"
+su -c "echo conservative > /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor"
+su -c "chmod 777 /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor"
+su -c "echo conservative > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor"
+su -c "chmod 777 /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor"
+su -c "echo conservative > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor"
 
-chmod 777 /sys/class/kgsl/kgsl-3d0/min_clock_mhz
-echo 275 > /sys/class/kgsl/kgsl-3d0/min_clock_mhz
-chmod 777 /sys/class/kgsl/kgsl-3d0/devfreq/governor
-a=`cat /sys/class/kgsl/kgsl-3d0/devfreq/governor`
+su -c "chmod 777 /sys/class/kgsl/kgsl-3d0/min_clock_mhz"
+su -c "echo 275 > /sys/class/kgsl/kgsl-3d0/min_clock_mhz"
+su -c "chmod 777 /sys/class/kgsl/kgsl-3d0/devfreq/governor"
+su -c "a=`cat /sys/class/kgsl/kgsl-3d0/devfreq/governor`"
 while [ $a != 'powersave' ]
 do
-   echo powersave > /sys/class/kgsl/kgsl-3d0/devfreq/governor
+   su -c "echo powersave > /sys/class/kgsl/kgsl-3d0/devfreq/governor"
    break
 done
 
-chmod 777 /sys/class/kgsl/kgsl-3d0/default_pwrlevel
-echo 3 > /sys/class/kgsl/kgsl-3d0/default_pwrlevel
+su -c "chmod 777 /sys/class/kgsl/kgsl-3d0/default_pwrlevel"
+su -c "echo 3 > /sys/class/kgsl/kgsl-3d0/default_pwrlevel"
 
 su -c "pm enable com.android.vending"
 su -c "pm enable com.android.deskclock"
