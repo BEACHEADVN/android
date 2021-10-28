e1d2f25b5fd8827f7e2adfe8677f369c8c81d9ee
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2021-10-28 09:09:30 +0700

    Add files via upload

---
+++ core.sh
@@ -0,0 +1,90 @@
+clear
+
+echo "Bạn muốn bật hay tắt nhân:"
+echo "1. BẬT"
+echo "2. TẮT"
+echo "Nhập số từ 1-2:"
+read onoff
+case $onoff in
+	1)
+		clear
+		echo "Chọn nhân muốn bật:"
+		echo "1. TOÀN BỘ"
+		echo "2. ĐƠN LẺ"
+		echo "Nhập số từ 1-2:"
+		read core
+		
+		case $core in
+			1)
+				x=0
+				while [ $x -le 7 ]
+				do
+					echo "Bật nhân $x"
+					su -c "chmod 777 /sys/devices/system/cpu/cpu$x/online"
+					su -c "echo 1 > /sys/devices/system/cpu/cpu$x/online"
+					su -c "chmod 444 /sys/devices/system/cpu/cpu$x/online"
+					x=$(( $x + 1 ))
+				done
+			;;
+			2)
+				clear
+				echo "Nhập số từ 0-7(không cần dấu cách, không cần theo thứ tự):"
+				read core1
+				value=$core1
+				arr=()
+				for ((i = 0; i < ${#value}; i++)); do
+				    arr+=(${value:$i:1})
+				done
+				
+				for x in "${arr[@]}"; do
+				    echo "Bật nhân $x"
+					su -c "chmod 777 /sys/devices/system/cpu/cpu$x/online"
+					su -c "echo 1 > /sys/devices/system/cpu/cpu$x/online"
+					su -c "chmod 444 /sys/devices/system/cpu/cpu$x/online"
+				done
+			;;
+		esac
+	;;
+	2)
+		clear
+		echo "Chọn nhân muốn tắt:"
+		echo "1. ĐA NHÂN(5-6-7)"
+		echo "2. ĐƠN LẺ"
+		echo "Nhập số từ 1-2:"
+		read core
+		
+		case $core in
+			1)
+				echo "Tắt nhân 7"
+				su -c "chmod 777 /sys/devices/system/cpu/cpu7/online"
+				su -c "echo 0 > /sys/devices/system/cpu/cpu7/online"
+				su -c "chmod 444 /sys/devices/system/cpu/cpu7/online"
+				echo "Tắt nhân 6"
+				su -c "chmod 777 /sys/devices/system/cpu/cpu6/online"
+				su -c "echo 0 > /sys/devices/system/cpu/cpu6/online"
+				su -c "chmod 444 /sys/devices/system/cpu/cpu6/online"
+				echo "Tắt nhân 5"
+				su -c "chmod 777 /sys/devices/system/cpu/cpu5/online"
+				su -c "echo 0 > /sys/devices/system/cpu/cpu5/online"
+				su -c "chmod 444 /sys/devices/system/cpu/cpu5/online"
+			;;
+			2)
+				clear
+				echo "Nhập số từ 0-7(không cần dấu cách, không cần theo thứ tự):"
+				read core1
+				value=$core1
+				arr=()
+				for ((i = 0; i < ${#value}; i++)); do
+				    arr+=(${value:$i:1})
+				done
+				
+				for x in "${arr[@]}"; do
+				    echo "Tắt nhân $x"
+					su -c "chmod 777 /sys/devices/system/cpu/cpu$x/online"
+					su -c "echo 0 > /sys/devices/system/cpu/cpu$x/online"
+					su -c "chmod 444 /sys/devices/system/cpu/cpu$x/online"
+				done
+			;;
+		esac
+	;;
+esac
