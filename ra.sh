d00a84023a103714110be17aa5b9ef738b57116d
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2024-06-30 19:43:15 +0700

    Add files via upload

--- ra.sh
+++ ra.sh
@@ -86,7 +86,7 @@ function vcb {
 	echo "•Bạn chọn Vietcombank"
 	name="Vietcombank"
 	package="com.VCB"
-	activity="com.VCB.activities.home.SplashActivity"
+	activity="com.VCB.ui.activities.home.SplashActivity"
 }
 
 function momo {
