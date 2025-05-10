2dfab83e5d81dc999b295f2af4b4d4ead199ee87
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2025-05-10 19:10:24 +0700

    Add files via upload

--- ra.sh
+++ ra.sh
@@ -73,7 +73,7 @@ function disable {
 
 if [ $a = on ]
 then
-	for i in hoyolab hackdata shopee adaway vcb momo photo x quickedit mtm tiktok galaxywearable myviettel mtmanager vneid instagram
+	for i in hoyolab shopee adaway vcb momo photo x quickedit mtm tiktok galaxywearable myviettel mtmanager vneid instagram
 	do
 		source /storage/emulated/0/Download/ext/temp/$i.sh
 		enable
@@ -87,7 +87,7 @@ fi
 if [[ $a = off || $a = of ]]
 then
 	rm -rf /storage/emulated/0/Download/ext/temp/vpndialog
-	for i in hoyolab hackdata shopee adaway vcb momo photo x quickedit mtm tiktok galaxywearable myviettel mtmanager vneid instagram
+	for i in hoyolab shopee adaway vcb momo photo x quickedit mtm tiktok galaxywearable myviettel mtmanager vneid instagram
 	do
 		source /storage/emulated/0/Download/ext/temp/$i.sh
 		disable
@@ -98,7 +98,7 @@ fi
 if [[ $a = off0 || $a = of0 ]]
 then
 	rm -rf /storage/emulated/0/Download/ext/temp/vpndialog
-	for i in hoyolab hackdata shopee adaway vcb momo photo x quickedit mtm tiktok galaxywearable myviettel mtmanager vneid instagram
+	for i in hoyolab shopee adaway vcb momo photo x quickedit mtm tiktok galaxywearable myviettel mtmanager vneid instagram
 	do
 		source /storage/emulated/0/Download/ext/temp/$i.sh
 		disable
