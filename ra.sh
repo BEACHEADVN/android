4a50b0d34892e9ddb7f3e91a087db8c71b8877fd
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2025-09-06 20:17:54 +0700

    Add files via upload

--- ra.sh
+++ ra.sh
@@ -86,16 +86,13 @@ then
 	su -c "am start --user 0 -n com.android.vending/com.android.vending.AssetBrowserActivity"
 	sleep 1
 	su -c "input swipe 1251 241 256 321 500"
-	su -c 'uiautomator dump ui.xml' >/dev/null 2>&1
+	sleep 1
 	python /data/data/com.termux/files/usr/bin/tap_avatar.py
 	sleep 1
-	su -c 'uiautomator dump ui.xml' >/dev/null 2>&1
 	python /data/data/com.termux/files/usr/bin/tap_text.py "Quản lý ứng dụng và thiết bị"
 	sleep 2
-	su -c 'uiautomator dump ui.xml' >/dev/null 2>&1
 	python /data/data/com.termux/files/usr/bin/tap_text.py "Có bản cập nhật"
 	sleep 1
-	su -c 'uiautomator dump ui.xml' >/dev/null 2>&1
 	python /data/data/com.termux/files/usr/bin/tap_text.py "Cập nhật tất cả"
 	python /data/data/com.termux/files/usr/bin/tap_text.py "Tất cả ứng dụng đều ở phiên bản mới nhất"
 	python /data/data/com.termux/files/usr/bin//tap.py "Kiểm tra để tìm bản cập nhật"
