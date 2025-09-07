e8db00933f5464c3b375617a092252ae58588d28
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2025-09-07 08:49:47 +0700

    Add files via upload

--- ra.sh
+++ ra.sh
@@ -91,11 +91,24 @@ then
 	sleep 1
 	python /data/data/com.termux/files/usr/bin/tap_text.py "Quản lý ứng dụng và thiết bị"
 	sleep 2
-	python /data/data/com.termux/files/usr/bin/tap_text.py "Có bản cập nhật"
-	sleep 1
-	python /data/data/com.termux/files/usr/bin/tap_text.py "Cập nhật tất cả"
-	python /data/data/com.termux/files/usr/bin/tap_text.py "Tất cả ứng dụng đều ở phiên bản mới nhất"
-	python /data/data/com.termux/files/usr/bin//tap.py "Kiểm tra để tìm bản cập nhật"
+	FILE="/data/data/com.termux/files/home/ui.xml"
+	if grep -q "Tất cả ứng dụng đều ở phiên bản mới nhất" "$FILE"
+	then
+		echo "Tất cả ứng dụng đều ở phiên bản mới nhất"
+		python /data/data/com.termux/files/usr/bin/tap_text.py "Tất cả ứng dụng đều ở phiên bản mới nhất"
+		sleep 1
+		python /data/data/com.termux/files/usr/bin//tap.py "Kiểm tra để tìm bản cập nhật"
+	elif grep -q "Có bản cập nhật" "$FILE"
+	then
+		echo "Có bản cập nhật"
+		python /data/data/com.termux/files/usr/bin/tap_text.py "Có bản cập nhật"
+		sleep 1
+		python /data/data/com.termux/files/usr/bin/tap_text.py "Cập nhật tất cả"
+	else
+		echo `date +"%r, %a, ngày %d, tháng %m, năm %Y"` >> /storage/emulated/0/Download/ext/temp/log.txt
+		sed -i 's/ PM,/ CH,/g; s/ AM,/ SA,/g; s/ Mon, / thứ Hai, /g; s/ Tue, / thứ Ba, /g; s/ Wed, / thứ Tư, /g; s/ Thur, / thứ Năm, /g; s/ Fri, / thứ Sáu, /g; s/ Sat, / thứ Bảy, /g; s/ Sun, / Chủ nhật, /g' >> /storage/emulated/0/Download/ext/temp/log.txt
+		echo "Không tìm thấy thông tin cập nhật Google Play" >> /storage/emulated/0/Download/ext/temp/log.txt
+	fi
 	rm -rf ui.xml
 	rm -rf /sdcard/ui.xml
 	ra
