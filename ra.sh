b7fe6d8277450646c7ce9e7b59a2cecb781c68d1
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2026-03-01 19:31:25 +0700

    Add files via upload

--- ra.sh
+++ ra.sh
@@ -51,9 +51,6 @@ do
 done
 
 echo ""
-echo "on    : Enable tất cả"
-echo "off   : Disable tất cả"
-echo "off0  : Disable tất cả + thoát"
 echo "0     : Thoát"
 echo ""
 read -p "Nhập lựa chọn: " a
@@ -68,6 +65,37 @@ then
         enable
     done
     echo "✓ Đã enable tất cả"
+    su -ic "am start --user 0 -n org.adaway/org.adaway.ui.home.HomeActivity"
+	sleep 2
+	su -ic "am start --user 0 -n com.android.vending/com.android.vending.AssetBrowserActivity"
+	sleep 1
+	su -ic "input swipe 1251 241 300 321 500"
+	sleep 1
+	python /data/data/com.termux/files/usr/bin/tap_avatar.py
+	sleep 1
+	python /data/data/com.termux/files/usr/bin/tap_text.py "Quản lý ứng dụng và thiết bị"
+	sleep 3
+	su -ic 'uiautomator dump ui.xml' > /dev/null 2>&1
+	FILE="/data/data/com.termux/files/home/ui.xml"
+	if grep -q "Tất cả ứng dụng đều ở phiên bản mới nhất" "$FILE"
+	then
+		echo "Tất cả ứng dụng đều ở phiên bản mới nhất"
+		python /data/data/com.termux/files/usr/bin/tap_text.py "Tất cả ứng dụng đều ở phiên bản mới nhất"
+		sleep 1
+		python /data/data/com.termux/files/usr/bin//tap.py "Kiểm tra để tìm bản cập nhật"
+	elif grep -q "Có bản cập nhật" "$FILE"
+	then
+		echo "Có bản cập nhật"
+		python /data/data/com.termux/files/usr/bin/tap_text.py "Có bản cập nhật"
+		sleep 1
+		python /data/data/com.termux/files/usr/bin/tap_text.py "Cập nhật tất cả"
+	else
+		echo `date +"%r, %a, ngày %d, tháng %m, năm %Y"` >> /storage/emulated/0/Download/ext/temp/log.txt
+		sed -i 's/ PM,/ CH,/g; s/ AM,/ SA,/g; s/ Mon, / thứ Hai, /g; s/ Tue, / thứ Ba, /g; s/ Wed, / thứ Tư, /g; s/ Thur, / thứ Năm, /g; s/ Fri, / thứ Sáu, /g; s/ Sat, / thứ Bảy, /g; s/ Sun, / Chủ nhật, /g' /storage/emulated/0/Download/ext/temp/log.txt
+		echo "Không tìm thấy thông tin cập nhật Google Play" >> /storage/emulated/0/Download/ext/temp/log.txt
+	fi
+	rm -rf ui.xml
+	rm -rf /sdcard/ui.xml
     ra
 fi
 
