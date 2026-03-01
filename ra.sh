f51b8e6a4e8b4b07f5c1035d273cf63a060fdf22
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2026-03-01 19:36:35 +0700

    Add files via upload

--- ra.sh
+++ ra.sh
@@ -1,71 +1,93 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-BASE="/storage/emulated/0/Download/ext/temp"
-LOG="$BASE/log.txt"
-
-# ===== DANH SÁCH APP =====
-apps=(
-hoyolab
-hackdata
-shopee
-adaway
-vcb
-momo
-photo
-revancify
-quickedit
-mtmanager
-vneid
-tiktok
-galaxywearable
-myviettel
-viettelmoney
-instagram
-x
-)
-
-# ===== FUNCTION =====
-enable() {
-    su -ic "pm enable $package"
-}
-
-disable() {
-    su -ic "pm disable $package"
-}
-
-run() {
-    su -ic "am start --user 0 -n $package/$activity"
-}
-
-log_time() {
-    echo `date +"%r, %a, ngày %d, tháng %m, năm %Y"` >> "$LOG"
-    sed -i 's/ PM,/ CH,/g; s/ AM,/ SA,/g; s/ Mon, / thứ Hai, /g; s/ Tue, / thứ Ba, /g; s/ Wed, / thứ Tư, /g; s/ Thur, / thứ Năm, /g; s/ Fri, / thứ Sáu, /g; s/ Sat, / thứ Bảy, /g; s/ Sun, / Chủ nhật, /g' "$LOG"
-}
-
-# ===== MENU =====
-clear
-echo "===== DANH SÁCH ỨNG DỤNG ====="
-for i in "${!apps[@]}"
-do
-    echo "$((i+1)): ${apps[$i]}"
-done
-
-echo ""
-echo "0     : Thoát"
-echo ""
-read -p "Nhập lựa chọn: " a
+cat <<EOT > /storage/emulated/0/Download/ext/temp/app
+Hoyo Lab
+Hack Data
+Shopee
+Adaway
+VCB
+MoMo
+Google Photo
+Revancify
+Quick Edit
+Mt Manager
+VNeID
+TikTok
+Galaxy Wearable
+My Viettel
+Viettel Money
+Instagram
+X
+EOT
 
 
-# ===== ENABLE ALL =====
-if [ "$a" = "on" ]
+
+if su -ic "find /data/app -type d -name 'com.google.ar.core*'" | grep -q .
 then
-    for i in "${apps[@]}"
-    do
-        source "$BASE/$i.sh"
-        enable
-    done
-    echo "✓ Đã enable tất cả"
-    su -ic "am start --user 0 -n org.adaway/org.adaway.ui.home.HomeActivity"
+	echo `date +"%r, %a, ngày %d, tháng %m, năm %Y"` >> /storage/emulated/0/Download/ext/temp/log.txt
+	sed -i 's/ PM,/ CH,/g; s/ AM,/ SA,/g; s/ Mon, / thứ Hai, /g; s/ Tue, / thứ Ba, /g; s/ Wed, / thứ Tư, /g; s/ Thur, / thứ Năm, /g; s/ Fri, / thứ Sáu, /g; s/ Sat, / thứ Bảy, /g; s/ Sun, / Chủ nhật, /g' /storage/emulated/0/Download/ext/temp/log.txt
+	echo "•Uninstall google ar core" >> /storage/emulated/0/Download/ext/temp/log.txt
+	su -ic "pm uninstall -k com.google.ar.core" 
+fi
+
+line_number=`wc -l /storage/emulated/0/Download/ext/temp/app | cut -d' ' -f1`
+line_number=$((line_number+1))
+
+clear
+echo "Chọn công việc:" 
+count=0
+while read line
+do
+	count=$((count+1))
+	echo "$count: $line"
+done < /storage/emulated/0/Download/ext/temp/app
+rm -rf /storage/emulated/0/Download/ext/temp/app
+
+echo "$line_number: Thoát"
+echo "Nhập số từ 1-$line_number:"
+read chon
+
+spacenumber=`echo "$chon" | awk -F" " '{print NF-1}'`
+if [ $spacenumber = 0 ]
+then
+	a=$(echo $chon | cut -d ' ' -f 1)
+	b=1
+	c=1
+elif [ $spacenumber = 1 ]
+then
+	a=$(echo $chon | cut -d ' ' -f 1)
+	b=$(echo $chon | cut -d ' ' -f 2)
+	c=1
+else
+	a=$(echo $chon | cut -d ' ' -f 1)
+	b=$(echo $chon | cut -d ' ' -f 2)
+	c=$(echo $chon | cut -d ' ' -f 3)
+fi
+
+function enable {
+	
+	su -ic "pm enable $package"
+	
+}
+function run {
+	
+	su -ic "am start --user 0 -n $package/$activity"
+	
+}
+
+function disable {
+	
+	su -ic "pm disable $package"
+	
+}
+
+if [ $a = on ]
+then
+	for i in hoyolab shopee vcb momo photo x quickedit tiktok galaxywearable myviettel mtmanager vneid instagram adaway
+	do
+		source /storage/emulated/0/Download/ext/temp/$i.sh
+		enable
+	done
+	
+	su -ic "am start --user 0 -n org.adaway/org.adaway.ui.home.HomeActivity"
 	sleep 2
 	su -ic "am start --user 0 -n com.android.vending/com.android.vending.AssetBrowserActivity"
 	sleep 1
@@ -96,51 +118,464 @@ then
 	fi
 	rm -rf ui.xml
 	rm -rf /sdcard/ui.xml
-    ra
+	
+	ra
 fi
 
-
-# ===== DISABLE ALL =====
-if [ "$a" = "off" ] || [ "$a" = "of" ]
+if [[ $a = off || $a = of ]]
 then
-    for i in "${apps[@]}"
-    do
-        source "$BASE/$i.sh"
-        disable
-    done
-    echo "✓ Đã disable tất cả"
-    ra
+	rm -rf /storage/emulated/0/Download/ext/temp/vpndialog
+	for i in hoyolab shopee vcb momo photo x quickedit tiktok galaxywearable myviettel mtmanager vneid instagram adaway
+	do
+		
+	source /storage/emulated/0/Download/ext/temp/$i.sh
+	if su -ic "pm list packages -d | grep -q $package"
+	then
+	    echo "  → $name đã bị disable."
+	else
+		echo "  → $name đang enable → disable..."
+	    disable
+	fi
+	
+	done
+	ra
 fi
 
-
-# ===== DISABLE ALL + KILL =====
-if [ "$a" = "off0" ] || [ "$a" = "of0" ]
+if [[ $a = off0 || $a = of0 ]]
 then
-    for i in "${apps[@]}"
-    do
-        source "$BASE/$i.sh"
-        disable
-    done
-    su -ic "killall -9 com.termux"
-    ra
+	rm -rf /storage/emulated/0/Download/ext/temp/vpndialog
+	for i in hoyolab shopee vcb momo photo x quickedit tiktok galaxywearable myviettel mtmanager vneid instagram adaway
+	do
+		
+		source /storage/emulated/0/Download/ext/temp/$i.sh
+		if su -ic "pm list packages -d | grep -q $package"
+		then
+			echo "  → $name đã bị disable."
+		else
+		    echo "  → $name đang enable → disable..."
+		    disable
+		fi
+		
+	done
+	su -ic "killall -9 com.termux"
 fi
 
-
-# ===== THOÁT =====
-if [ "$a" = "0" ]
+if [[ "$a" =~ ^[0-9]+$ ]] && [ "$a" -ge 1 ] && [ "$a" -le 18 ]
 then
-    exit
-fi
-
-
-# ===== CHỌN THEO SỐ =====
-if [[ "$a" =~ ^[0-9]+$ ]] && [ "$a" -ge 1 ] && [ "$a" -le ${#apps[@]} ]
-then
-    appname=${apps[$((a-1))]}
-    source "$BASE/$appname.sh"
-    enable
-    run
-    ra
+    echo ""
 else
-    echo "Lựa chọn không hợp lệ"
+    ra
+fi
+
+if [ "$b" -eq 1 ] || [ "$b" -eq 2 ] || [ "$b" -eq 0 ]
+then
+    echo ""
+else
+    ra
+fi
+
+if [ "$c" -eq 1 ] || [ "$c" -eq 0 ]
+then
+    echo ""
+else
+    ra
+fi
+
+if [ $a = 1 ]
+then
+	source /storage/emulated/0/Download/ext/temp/hoyolab.sh
+	if [ $b = 1 ]
+	then
+		enable
+		run
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	else
+		disable
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	fi
+fi
+
+if [ $a = 2 ]
+then
+	source /storage/emulated/0/Download/ext/temp/hackdata.sh
+	if [ $b = 1 ]
+	then
+		enable
+		run
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	else
+		disable
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	fi
+fi
+
+if [ $a = 3 ]
+then
+	source /storage/emulated/0/Download/ext/temp/shopee.sh
+	if [ $b = 1 ]
+	then
+		enable
+		run
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	else
+		disable
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	fi
+fi
+
+if [ $a = 4 ]
+then
+	source /storage/emulated/0/Download/ext/temp/adaway.sh
+	if [ $b = 1 ]
+	then
+		enable
+		run
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	else
+		disable
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	fi
+fi
+
+if [ $a = 5 ]
+then
+	source /storage/emulated/0/Download/ext/temp/vcb.sh
+	if [ $b = 1 ]
+	then
+		enable
+		run
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	else
+		disable
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	fi
+fi
+
+if [ $a = 6 ]
+then
+	source /storage/emulated/0/Download/ext/temp/momo.sh
+	if [ $b = 1 ]
+	then
+		enable
+		run
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	else
+		disable
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	fi
+fi
+
+if [ $a = 7 ]
+then
+	source /storage/emulated/0/Download/ext/temp/photo.sh
+	if [ $b = 1 ]
+	then
+		enable
+		run
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	else
+		disable
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	fi
+fi
+
+if [ $a = 9 ]
+then
+	source /storage/emulated/0/Download/ext/temp/quickedit.sh
+	if [ $b = 1 ]
+	then
+		enable
+		run
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	else
+		disable
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	fi
+fi
+
+if [ $a = 10 ]
+then
+	source /storage/emulated/0/Download/ext/temp/mtmanager.sh
+	if [ $b = 1 ]
+	then
+		enable
+		run
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	else
+		disable
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	fi
+fi
+
+if [ $a = 11 ]
+then
+	source /storage/emulated/0/Download/ext/temp/vneid.sh
+	if [ $b = 1 ]
+	then
+		enable
+		run
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	else
+		disable
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	fi
+fi
+
+if [ $a = 12 ]
+then
+	source /storage/emulated/0/Download/ext/temp/tiktok.sh
+	if [ $b = 1 ]
+	then
+		enable
+		run
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	else
+		disable
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	fi
+fi
+
+if [ $a = 13 ]
+then
+	source /storage/emulated/0/Download/ext/temp/galaxywearable.sh
+	if [ $b = 1 ]
+	then
+		enable
+		run
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	else
+		disable
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	fi
+fi
+
+if [ $a = 14 ]
+then
+	source /storage/emulated/0/Download/ext/temp/myviettel.sh
+	if [ $b = 1 ]
+	then
+		enable
+		run
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	else
+		disable
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	fi
+fi
+
+if [ $a = 15 ]
+then
+	source /storage/emulated/0/Download/ext/temp/viettelmoney.sh
+	if [ $b = 1 ]
+	then
+		enable
+		run
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	else
+		disable
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	fi
+fi
+
+if [ $a = 16 ]
+then
+	source /storage/emulated/0/Download/ext/temp/instagram.sh
+	if [ $b = 1 ]
+	then
+		enable
+		run
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	else
+		disable
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	fi
+fi
+
+if [ $a = 17 ]
+then
+	source /storage/emulated/0/Download/ext/temp/x.sh
+	if [ $b = 1 ]
+	then
+		enable
+		run
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	else
+		disable
+		if [ $c = 1 ]
+		then
+			ra
+		else
+			su -ic "killall -9 com.termux"
+		fi
+	fi
+fi
+
+if [ $a = 18 ]
+then
+	if [ $b = 0 ]
+	then
+		su -ic "killall -9 com.termux"
+	else
+		clear
+	fi
+fi
+
+if [ $a = 8 ]
+then
+	 bash /storage/emulated/0/Download/ext/temp/revancify_mod.sh
 fi
