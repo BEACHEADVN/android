459aab9462bc36fc6c0560a1ebf9b074aae5c6c7
Lê Văn Việt <37505952+BEACHEADVN@users.noreply.github.com>
2026-05-05 19:56:44 +0700

    Add files via upload

--- ra.sh
+++ ra.sh
@@ -18,20 +18,43 @@ Instagram
 X
 EOT
 
+PKGS="
+com.google.ar.core
+com.miui.miwallpaper.moon
+com.miui.miwallpaper.earth
+com.miui.miwallpaper.geometry
+com.miui.miwallpaper.mars
+com.miui.miwallpaper.saturn
+com.miui.miwallpaper.snowmountain
+"
 
+FOUND=false
 
-if su -c "find /data/app -type d -name 'com.google.ar.core*'" | grep -q .
+for pkg in $PKGS
+do
+    if su -c "pm list packages | grep -q $pkg"
+    then
+        FOUND=true
+        su -c "pm uninstall -k $pkg"
+        echo "•Uninstall $pkg" >> /storage/emulated/0/Download/ext/temp/log.txt
+    fi
+done
+
+if [ "$FOUND" = true ]
 then
-	echo `date +"%r, %a, ngày %d, tháng %m, năm %Y"` >> /storage/emulated/0/Download/ext/temp/log.txt
-	sed -i 's/ PM,/ CH,/g; s/ AM,/ SA,/g; s/ Mon, / thứ Hai, /g; s/ Tue, / thứ Ba, /g; s/ Wed, / thứ Tư, /g; s/ Thur, / thứ Năm, /g; s/ Fri, / thứ Sáu, /g; s/ Sat, / thứ Bảy, /g; s/ Sun, / Chủ nhật, /g' /storage/emulated/0/Download/ext/temp/log.txt
-	echo "•Uninstall google ar core" >> /storage/emulated/0/Download/ext/temp/log.txt
-	su -c "pm uninstall -k com.google.ar.core"
-	su -c "pm uninstall -k com.miui.miwallpaper.moon"
-	su -c "pm uninstall -k com.miui.miwallpaper.earth"
-	su -c "pm uninstall -k com.miui.miwallpaper.geometry"
-	su -c "pm uninstall -k com.miui.miwallpaper.mars"
-	su -c "pm uninstall -k com.miui.miwallpaper.saturn"
-	su -c "pm uninstall -k com.miui.miwallpaper.snowmountain"
+    echo `date +"%r, %a, ngày %d, tháng %m, năm %Y"` >> /storage/emulated/0/Download/ext/temp/log.txt
+
+    sed -i '
+    s/ PM,/ CH,/g;
+    s/ AM,/ SA,/g;
+    s/ Mon, / thứ Hai, /g;
+    s/ Tue, / thứ Ba, /g;
+    s/ Wed, / thứ Tư, /g;
+    s/ Thu, / thứ Năm, /g;
+    s/ Fri, / thứ Sáu, /g;
+    s/ Sat, / thứ Bảy, /g;
+    s/ Sun, / Chủ nhật, /g
+    ' /storage/emulated/0/Download/ext/temp/log.txt
 fi
 
 line_number=`wc -l /storage/emulated/0/Download/ext/temp/app | cut -d' ' -f1`
